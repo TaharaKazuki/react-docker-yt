040ed9c9cee84383551d75fcefc47ec8a28002bf
田原 一樹 <ssp0727.lnc@gmail.com>
2021-08-18 21:09:08 +0900

    add env

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{`Hello ${process.env.REACT_APP_NAME}`}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,7 +20,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
